7b27eb9bb9cbe241ed98b4e196e301c50e0604ea
ibhavikmakwana <bhavikmakwana43@gmail.com>
2019-04-10 11:57:23 +0530

    Data Table example

--- lib/ui/datatable/data_table_example.dart
+++ lib/ui/datatable/data_table_example.dart
@@ -9,7 +9,65 @@ class DataClassExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Title(color: Colors.amber, child: Text("hefdnf")),
+        title: Text(title),
+      ),
+      body: Center(
+        child: DataTable(
+          sortColumnIndex: 1,
+          columns: [
+            DataColumn(
+                label: Text("One"),
+                numeric: true,
+                tooltip: "This is first column"),
+            DataColumn(
+              label: Text("Two"),
+              numeric: false,
+            )
+          ],
+          rows: [
+            DataRow(
+              cells: [
+                DataCell(
+                  Text("Cell 1"),
+                  showEditIcon: true,
+                  onTap: () {
+                    print("Cell 1 tapped");
+                  },
+                  placeholder: false,
+                ),
+                DataCell(
+                  Text("Cell 2"),
+                  onTap: () {},
+                ),
+              ],
+            ),
+            DataRow(
+              cells: [
+                DataCell(
+                  Text("Cell 3"),
+                  onTap: () {},
+                ),
+                DataCell(
+                  Text("Cell 4"),
+                  onTap: () {},
+                ),
+              ],
+            ),
+            DataRow(
+              cells: [
+                DataCell(
+                  Text("Cell 5"),
+                  onTap: () {},
+                ),
+                DataCell(
+                  Text("Cell 6"),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ],
+          sortAscending: true,
+        ),
       ),
     );
   }
