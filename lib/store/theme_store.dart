2537e37e8a06e2484848e0b8b8c8592041b5b4f7
bhavik <bhavikmakwana43@gmail.com>
2020-01-14 14:47:47 +0530

    :sparkles: Implemented a Dark Mode, :sparkles: Adding MobX for State Management

---
+++ lib/store/theme_store.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:mobx/mobx.dart';
+
+part 'theme_store.g.dart';
+
+class ThemeStore = _ThemeStore with _$ThemeStore;
+
+abstract class _ThemeStore with Store {
+  @observable
+  ThemeMode themeMode = ThemeMode.light;
+
+  @action
+  changeTheme() {
+    themeMode == ThemeMode.light
+        ? themeMode = ThemeMode.dark
+        : themeMode = ThemeMode.light;
+  }
+}
